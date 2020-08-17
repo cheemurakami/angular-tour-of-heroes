d7333e42bc38e34395541b9c6e8bfd02fa05ced5
Chisato Murakami <kiwipom1031@gmail.com>
2020-08-16 21:23:06 -0700

    add OnSelect method

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -8,18 +8,37 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // hero = "Windstorm";
-
-  // hero: Hero = {
-  //   id: 1,
-  //   name: "Kiwi"
-  // };
 
   heroes = HEROES;
-
+  selectedHero: Hero;
+  
   constructor() { }
 
   ngOnInit(): void {
+    let result = this.getPrice();
+
+    result.id 
+
   }
 
+  getPrice(): Hero {
+    return {
+      id: 9,
+      name: "hello"
+    }
+  }
+
+  onSelect(hero: Hero): void{ 
+    this.selectedHero = hero;
+  }
 }
+
+
+// interface FooResponse
+// {
+//   id: number;
+//   name: string;
+//   price: number;
+// }
+
+//GET  http://localhost:8000/api/price
